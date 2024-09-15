f8f5dd093ded2d10feb5fe4c6e99885ee425d109
mohamed <simonbakhcha@gmail.com>
2024-09-15 15:27:20 +0100

    upd

--- func/style.go
+++ func/style.go
@@ -2,11 +2,19 @@ package groupie
 
 import (
 	"net/http"
+	"os"
 )
 
 func Style(w http.ResponseWriter, r *http.Request) {
+	f := r.PathValue("file")
 	style := http.StripPrefix("/style/", http.FileServer(http.Dir("style")))
+	_, err := os.ReadFile("style/" + f)
+
+	if err != nil {
+		Error(w, http.StatusNotFound)
+		return
+	}
 
 	style.ServeHTTP(w, r)
-
+	
 }
--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 
 func main() {
 	groupie.Isfitch = groupie.Fetch("artists", "") // get ipa data and fitched
-	http.HandleFunc("/style/style.css", groupie.Style)
-	http.HandleFunc("/style/background.png.jpg", groupie.Style)
+	http.HandleFunc("/style/{file}", groupie.Style)
 	http.HandleFunc("/", groupie.Gethandel)
 	http.HandleFunc("/artist/{id}", groupie.Posthandel)
-	fmt.Println("http://localhost:5461")
-	log.Fatal(http.ListenAndServe(":5461", nil))
+	fmt.Println("http://localhost:4061")
+	log.Fatal(http.ListenAndServe(":4061", nil))
 
 }
